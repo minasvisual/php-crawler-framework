8813a506c4f21e80c77b55439d373d598516c9dc
Ulisses <mantovaniarts@hotmail.com>
2020-05-05 21:36:10 -0300

    examples added

---
+++ app/examples/example.php
@@ -0,0 +1,73 @@
+<?php
+use \Core\Helpers;
+
+$helpers = new Helpers(null);
+
+$helpers->setConfig('debug', true);
+$helpers->setConfig('logging', true);
+
+return (object) [
+  "name" => "example",
+  "status" => true,
+  "debug" => true,
+  "log" => true,
+  "schedule" => '0 */2 * * * *',
+  "header" => [],
+  "tasks" => [
+      ["url" => "https://ionicabizau.net"],
+      ["task" => function($response) use ($helpers){
+        $helpers->inspect("Called task all ".$response['model']->name ); 
+        return 'OK';
+      } ],
+  ],
+  "schema" => [
+      "title" => ".header h1",
+      "desc" => [
+        "selector"=>".header h2",
+        "convert" => function($x){ return trim($x); }
+      ],
+      "avatar" => [
+        "selector" => ".header img",
+        "attr" => "src"
+      ],
+      "pages" => [
+          'listItem' => ".pages .page a"
+      ],
+      "active" => [
+          'selector' => ".pages .page a",
+          'child' => 2, // starts by 0
+          'trim' => true,
+          'attr' => 'href'
+      ],
+      "nav" => [
+            'listItem' => ".pages .page",
+            'data'=> [
+                'title'=> "a",
+                'url'=> [
+                    'selector'=> "a",
+                    'attr'=> "href"
+                ]
+            ]
+        ],
+      "header" => [
+          "selector" => ".header h1",
+          "how" => "html"
+      ],
+  ],
+  "beforeAll" => function($model) use ($helpers){
+    $helpers->inspect("Called before all");
+    return $model;
+  },
+  "success" => function($response) use ($helpers){
+     $helpers->inspect($response['data']);
+  },
+  "error" => function($err) use ($helpers){
+    $helpers->inspect($response);
+  },
+  "afterAll" => function($data) use ($helpers){
+    $helpers->inspect("Called after all");
+    
+    return $data;
+  }
+  // export
+];
